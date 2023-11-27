9183e47951a04267f6303134d9be5efea2e35fbf
Programador Cego <programadorcego@gmail.com>
2023-11-27 07:19:58 -0300

    Método where

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 	public function index()
 	{
 		$title = "Usuários";
-		$users = User::all();
+		$users = User::where(['id' => 10])->get();
 		return view("admin.users.index", compact("title", "users"));
 	}
 	
